b857dcc061030d473d8d412c2db579c16ac46c2a
Yufeng Zhao <yufeng-zhao@outlook.com>
2025-03-15 19:51:48 -0400

    add empty state

--- src/components/billiard-ball.tsx
+++ src/components/billiard-ball.tsx
@@ -1,9 +1,10 @@
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 
-interface LoadingBallProps {
+interface BilliardBallProps {
   className?: string;
   ballType?: keyof typeof BALL_TYPE_IMAGES;
+  spin?: boolean;
 }
 
 const BALL_TYPE_IMAGES = {
@@ -11,16 +12,20 @@ const BALL_TYPE_IMAGES = {
   "cue-ball": "/balls/cue-ball.svg",
 } as const;
 
-function LoadingBall({ className, ballType = "cue-ball" }: LoadingBallProps) {
+function BilliardBall({
+  className,
+  ballType = "cue-ball",
+  spin = false,
+}: BilliardBallProps) {
   return (
     <Image
       src={BALL_TYPE_IMAGES[ballType]}
       alt="Loading ball"
       width={16}
       height={16}
-      className={cn("h-16 w-16 animate-spin", className)}
+      className={cn("h-16 w-16", spin && "animate-spin", className)}
     />
   );
 }
 
-export default LoadingBall;
+export default BilliardBall;
--- src/components/collectable-grid.tsx
+++ src/components/collectable-grid.tsx
@@ -3,7 +3,7 @@
 import { api } from "@/trpc/react";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import type { api as serverApi } from "@/trpc/server";
-import { Suspense, useEffect, useState } from "react";
+import { Suspense, useEffect, useMemo, useState } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { motion } from "motion/react";
 
@@ -12,7 +12,7 @@ import {
   useCollectableFilterParams,
 } from "@/hooks/params-parsers/use-collectable-filter-params";
 import { CollectableCard } from "./collectable-card";
-import LoadingBall from "./loading-ball";
+import BilliardBall from "./billiard-ball";
 interface CollectableGridProps {
   initialData: Awaited<
     ReturnType<(typeof serverApi)["collectable"]["getInfiniteScroll"]>
@@ -45,15 +45,19 @@ function CollectableGrid({ initialData, pageSize }: CollectableGridProps) {
       },
     );
 
+  const allItems = useMemo(() => {
+    return infiniteCollectables.data?.pages.flatMap((page) => page.items);
+  }, [infiniteCollectables.data]);
+
   return (
     <>
       {infiniteCollectables.isLoading && (
-        <div className="flex h-[calc(100vh-36rem)] items-center justify-center">
-          <LoadingBall className="mt-12" />
+        <div className="flex h-[calc(100vh-32rem)] items-center justify-center">
+          <BilliardBall className="" spin />
         </div>
       )}
 
-      {!infiniteCollectables.isLoading && (
+      {!infiniteCollectables.isLoading && allItems?.length > 0 && (
         <InfiniteScroll
           className="pb-24"
           dataLength={infiniteCollectables.data?.pages.length ?? 0}
@@ -63,25 +67,30 @@ function CollectableGrid({ initialData, pageSize }: CollectableGridProps) {
           style={{ overflow: "visible" }}
           loader={
             <div className="flex items-center justify-center">
-              <LoadingBall className="mt-12" />
+              <BilliardBall className="mt-12" spin />
             </div>
           }
         >
           <div className="grid grid-cols-2 gap-x-6 gap-y-12 md:grid-cols-3 lg:grid-cols-4">
-            {infiniteCollectables.data?.pages.map((page) =>
-              page.items.map((item, i) => (
-                <motion.div
-                  key={item.id}
-                  initial={{ opacity: 0, y: 10 }}
-                  animate={{ opacity: 1, y: 0 }}
-                  transition={{ duration: 0.5, delay: i * 0.1 }}
-                >
-                  <CollectableCard key={item.id} collectable={item} />
-                </motion.div>
-              )),
-            )}
+            {allItems?.map((item, i) => (
+              <motion.div
+                key={item.id}
+                initial={{ opacity: 0, y: 10 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5, delay: i * 0.1 }}
+              >
+                <CollectableCard key={item.id} collectable={item} />
+              </motion.div>
+            ))}
           </div>
         </InfiniteScroll>
+      )}
+
+      {!infiniteCollectables.isLoading && allItems?.length === 0 && (
+        <div className="flex h-[calc(100vh-32rem)] flex-col items-center justify-center gap-4">
+          <BilliardBall className="" ballType="8-ball" />
+          <p className="text-2xl">No results found</p>
+        </div>
       )}
     </>
   );
